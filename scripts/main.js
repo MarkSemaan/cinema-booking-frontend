f6fc1baa5a9d43656e1464a4f3c879cc0bd24127
MarkSemaan <mark.h.semaan@gmail.com>
2025-06-29 22:25:55 +0300

    Added button to test axios api calling

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("btn").addEventListener("click", () => {
+  document.getElementById("btn-view-movies").addEventListener("click", () => {
     axios
       .get("http://localhost/cinema-booking-backend/api/movies.php?action=list")
       .then((response) => {
         console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching movies:", error);
       });
   });
 });
